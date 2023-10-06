0a92a6c91d3a70caecf9e66ec1ebeb57c9f76a7c
saadahmad0 <147067305+saadahmad0@users.noreply.github.com>
2023-10-06 17:46:05 +0500

    Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+public class Calulator
+{
+    public static void Main()
+    {
+        Console.Write("Enter First Number: ");
+        int num1= Convert.ToInt32(Console.ReadLine());
+ 
+        Console.Write("Enter Second number: ");
+        int num2= Convert.ToInt32(Console.ReadLine());
+   
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1+num2);
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1-num2);
+        Console.WriteLine("{0} x {1} = {2}", num1, num2, num1*num2);
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1/num2);
+        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1%num2);
+    }
+}
